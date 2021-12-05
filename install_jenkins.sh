36543c5cf62cd3f299b28d982b7b217d67b8e44e
joshking1 <josh.kidfileapp@gmail.com>
2021-12-05 11:16:10 -0500

    new terraform for advanced container

---
+++ install_jenkins.sh
@@ -0,0 +1,52 @@
+
+
+#!/bin/bash
+sudo yum -y update
+
+echo "Install Java JDK 8"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+
+echo "Install Maven"
+yum install -y maven 
+
+echo "Install git"
+yum install -y git
+
+echo "Install Docker engine"
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+systemctl status docker
+#sudo usermod -a -G docker jenkins
+#sudo service docker start
+sudo chkconfig docker on
+
+echo "Install Jenkins"
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  --name myjenkin \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkinsci/blueocean
+docker exec -it myjenkin bash
+cat /var/jenkins_home/secrets/initialAdminPassword
+exit
+docker logs myjenkin
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install jenkins -y
+amazon-linux-extras install epel -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+java -jar jenkins.war
+
+
