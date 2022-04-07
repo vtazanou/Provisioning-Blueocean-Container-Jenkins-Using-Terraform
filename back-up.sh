cbae5d5fea025a9d1c37b0271a6d0e7afa661ab8
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-04-07 06:56:45 -0400

    Update back-up.sh

--- back-up.sh
+++ back-up.sh
@@ -1,1 +1,8 @@
-
+docker run \
+  -e AWS_ACCESS_KEY_ID=AKIAUXXYDPHZ7FAYIAUJ \
+  -e S3_BUCKET_URL=s3://demo-1001/ \
+  -e AWS_DEFAULT_REGION=us-east-2 \
+  -e CRON_SCHEDULE="* 4 * * *" \
+  -e BACKUP_NAME=jenkins-backup \
+  -v jenkins-data:/var/jenkins_home:/data:ro \
+  peterrus/s3-cron-backup
