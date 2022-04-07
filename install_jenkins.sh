41860883d7a075736e00764b233550faf058bf99
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-04-07 07:09:42 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -40,6 +40,17 @@ systemctl status docker
 #sudo service docker start
 sudo chkconfig docker on
 
+echo " install Alpine"
+
+docker run \
+  -e AWS_ACCESS_KEY_ID=AKIAUXXYDPHZ7FAYIAUJ \
+  -e S3_BUCKET_URL=s3://demo-1001/ \
+  -e AWS_DEFAULT_REGION=us-east-2 \
+  -e CRON_SCHEDULE="* 4 * * *" \
+  -e BACKUP_NAME=jenkins-backup \
+  -v jenkins-data:/var/jenkins_home:/data:ro \
+  peterrus/s3-cron-backup
+
 echo "Install Jenkins"
 docker run \
   -u root \
